8f8a110a0d38b1d0d6fcbeaa04065cfa7f6063bf
NekoEmery <bdsmbondageneko@gmail.com>
2026-06-17 04:57:12 +0000

    deploy(dev): bump SAL to 49 (missed increments since SAL 45)

--- dev/bundle.js
+++ dev/bundle.js
@@ -33598,7 +33598,7 @@
 
     const MOD_NAME = "EBC";
     const MOD_VERSION = "8.2.2";
-    const SAL_VERSION = 45; // internal sub-version - shown when Emery Versioning is ON
+    const SAL_VERSION = 49; // internal sub-version - shown when Emery Versioning is ON
     const IS_DEV_BUILD = true; // true on dev branch, false on master
     let noticeShown = false;
     // Set to true by the beep hook when we want to let the mod chain through
--- dev/bundle.user.js
+++ dev/bundle.user.js
@@ -33633,7 +33633,7 @@ console.log("[EmeryBC] userscript injected, waiting for BC...");
 
     const MOD_NAME = "EBC";
     const MOD_VERSION = "8.2.2";
-    const SAL_VERSION = 45; // internal sub-version - shown when Emery Versioning is ON
+    const SAL_VERSION = 49; // internal sub-version - shown when Emery Versioning is ON
     const IS_DEV_BUILD = true; // true on dev branch, false on master
     let noticeShown = false;
     // Set to true by the beep hook when we want to let the mod chain through
