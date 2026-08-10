a7412c1f5df39b38dd8060dc43d4d957c914dada
NekoEmery <269762296+NekoEmery@users.noreply.github.com>
2026-08-10 12:28:50 +0000

    deploy(dev): Back to a dev build after the 9.0.6 release

--- dev/bundle.user.js
+++ dev/bundle.user.js
@@ -42014,7 +42014,7 @@ This cannot be undone.`, "Cancel", "Delete", () => { clearDataCategory(cat); thi
 
     const MOD_NAME = "EBC";
     const MOD_VERSION = "9.0.6";
-    const SAL_VERSION = 294; // internal sub-version - shown when Emery Versioning is ON
+    const SAL_VERSION = 295; // internal sub-version - shown when Emery Versioning is ON
     const IS_DEV_BUILD = true; // true on dev branch, false on master
     let noticeShown = false;
     // Set to true by the beep hook when we want to let the mod chain through
